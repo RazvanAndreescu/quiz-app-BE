708fb11c2fd71206394c4b08f0bab2589a3b30ac
Razvan Andreescu <andreescurazvan.18@yahoo.com>
2023-01-07 16:18:54 +0200

    CORS error FIX

--- src/main/java/com/quizapp/quizappbe/controller/ExamController.java
+++ src/main/java/com/quizapp/quizappbe/controller/ExamController.java
@@ -4,11 +4,9 @@ import com.quizapp.quizappbe.infrastructure.dto.ExamDto;
 import com.quizapp.quizappbe.service.ExamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("http://localhost:4200/")
 @RestController
 public class ExamController {
 
